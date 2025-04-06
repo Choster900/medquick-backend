362645755127fd0ade3f9594bc360006f630744d
Choster900 <16adonaysergio@gmail.com>
2025-04-06 12:19:25 -0600

    feat: enhance main application setup with global prefix and validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+    const logger = new Logger('Main App');
+
+
+    const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('api');
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+        }),
+    );
+    await app.listen(envs.PORT);
+
+
+
+    logger.log("Backend ready for using PORT: " + envs.PORT)
 }
 bootstrap();
