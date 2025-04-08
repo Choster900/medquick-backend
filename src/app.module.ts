060a2513540c7586ae5b140e64b86464782683dd
Sergio.Lopez <salopez@innovacion.gob.sv>
2025-04-08 13:13:17 -0600

    feat: add modules, controllers, services, and DTOs for appointments, branches, exams, files, institutions, permissions, prescriptions, specialties, and users
    - Created AppointmentsModule with controller and service
    - Created BranchesModule with controller and service
    - Created ExamsModule with controller and service
    - Created FilesModule with controller and service
    - Created InstitutionsModule with controller and service
    - Created PermissionsModule with controller and service
    - Created PrescriptionsModule with controller and service
    - Created SpecialtiesModule with controller and service
    - Created UsersModule with controller and service
    - Added corresponding DTOs and entities for each module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { InstitutionsModule } from './institutions/institutions.module';
+import { BranchesModule } from './branches/branches.module';
+import { SpecialtiesModule } from './specialties/specialties.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { PrescriptionsModule } from './prescriptions/prescriptions.module';
+import { ExamsModule } from './exams/exams.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { FilesModule } from './files/files.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, InstitutionsModule, BranchesModule, SpecialtiesModule, AppointmentsModule, PrescriptionsModule, ExamsModule, PermissionsModule, FilesModule, UsersModule],
   controllers: [],
   providers: [],
 })
