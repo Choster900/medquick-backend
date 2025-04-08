be7fa28fd49045afa98282505d89375b4fce2afe
Sergio.Lopez <salopez@innovacion.gob.sv>
2025-04-08 09:02:02 -0600

    feat(auth): enhance user registration API with Swagger documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto';
 import { LoginUserByEmailDto } from './dto/login-user-email.dto';
+import { ApiResponse } from '@nestjs/swagger';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
     @Post('register')
+    @ApiResponse({ status: 201, description: 'The user has been successfully registered.', type: User })
+    @ApiResponse({ status: 400, description: 'Bad Request: Validation errors or invalid data.' })
+    @ApiResponse({ status: 500, description: 'Internal Server Error: Something went wrong.' })
     create(@Body() registerUserDto: RegisterUserDto) {
         return this.authService.registerUser(registerUserDto);
     }
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,23 +1,46 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Gender } from "@prisma/client";
+
 export class User {
 
+    @ApiProperty({
+        example: 'aa994459-8fd2-4fde-b355-8b5d766d042b'
+    })
     user_id: number;
+    @ApiProperty()
     branch_id: number | null;
+    @ApiProperty()
     id_institucion: number | null;
+    @ApiProperty()
     user_state_id: number;
-    user_gender: boolean;
-
+    @ApiProperty()
+    user_gender: Gender;
+    @ApiProperty()
     user_first_name: string;
+    @ApiProperty()
     user_second_name: string;
+    @ApiProperty()
     user_third_name: string;
+    @ApiProperty()
     user_first_lastname: string;
+    @ApiProperty()
     user_second_lastname: string;
+    @ApiProperty()
     user_third_lastname: string;
+    @ApiProperty()
     user_email: string;
+    @ApiProperty()
     user_password: string;
+    @ApiProperty()
     user_dui: string;
+    @ApiProperty()
     user_birthdate: Date;
+    @ApiProperty()
     user_phone_number: string;
+    @ApiProperty()
     user_address: string;
+    @ApiProperty()
     user_create_at: Date;
+    @ApiProperty()
     user_update_at: Date;
 }
