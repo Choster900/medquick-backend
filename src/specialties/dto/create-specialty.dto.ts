1900253982b5028de57cbef60769c367e66d4d6e
Choster900 <16adonaysergio@gmail.com>
2025-04-09 14:17:31 -0600

    feat(database): add migration for administration_route, prescription, prescription_item, specialty, and users_branches_specialty tables; update schema.prisma and create-specialty DTO

--- src/specialties/dto/create-specialty.dto.ts
+++ src/specialties/dto/create-specialty.dto.ts
@@ -1,1 +1,11 @@
-export class CreateSpecialtyDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateSpecialtyDto {
+    @IsNotEmpty()
+    @IsString()
+    specialty_name: string;
+
+    @IsOptional()
+    @IsString()
+    specialty_description?: string;
+}
