ea5fbccd13ebb269db001154e8931a8a1817d8db
Choster900 <16adonaysergio@gmail.com>
2025-04-10 11:33:21 -0600

    feat(database): update schema to include cascading delete on relations; add new seeders for administration routes, file types, medical appointment states, and relationship types

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,14 +3,24 @@ import { seedUserStates } from './seeders/user-states.seeder';
 import { seedUsers } from './seeders/users.seeder';
 import { seedInstitutions } from './seeders/institutions.seeder';
 import { seedBranches } from './seeders/branches.seeder';
+import { seedMedicalAppointmentStates } from './seeders/medical_appointment_state';
+import { seedAdministrationRoutes } from './seeders/administration_route';
+import { seedFileTypes } from './seeders/file_type';
+import { seedRelationshipTypes } from './seeders/relationship_type';
 
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.user.deleteMany();
+    await prisma.medical_appointment.deleteMany();
     await prisma.user_state.deleteMany();
     await prisma.branch.deleteMany();
     await prisma.institution.deleteMany();
+    await prisma.user.deleteMany();
+    await prisma.medical_appointment_state.deleteMany();
+    await prisma.administration_route.deleteMany();
+    await prisma.file_type.deleteMany();
+    await prisma.relationship_type.deleteMany();
+
 
     const now = new Date();
 
@@ -18,8 +28,13 @@ async function main() {
     await seedUsers(prisma);
     await seedInstitutions(prisma, now);
     await seedBranches(prisma, now);
+    await seedMedicalAppointmentStates(prisma, now);
+    await seedAdministrationRoutes(prisma, now);
+    await seedFileTypes(prisma, now);
+    await seedRelationshipTypes(prisma, now);
 
     console.log('✅ Seed ejecutado correctamente.');
+
 }
 
 main()
---
+++ prisma/seeders/medical_appointment_state.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+
+export async function seedMedicalAppointmentStates(prisma: PrismaClient, now: Date) {
+
+    await prisma.$executeRawUnsafe(`DELETE FROM sqlite_sequence WHERE name = 'medical_appointment_state';`);
+
+    await prisma.medical_appointment_state.createMany({
+        data: [
+            { medical_appointment_state_description: 'Scheduled', medical_appointment_state_created_at: now },
+            { medical_appointment_state_description: 'Completed', medical_appointment_state_created_at: now },
+            { medical_appointment_state_description: 'Cancelled', medical_appointment_state_created_at: now },
+            { medical_appointment_state_description: 'No Show', medical_appointment_state_created_at: now }
+        ]
+    })
+}
