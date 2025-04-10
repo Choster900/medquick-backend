fe5291694f52fc5ade5c65963425a5353cb8e5af
Choster900 <16adonaysergio@gmail.com>
2025-04-10 14:13:59 -0600

    feat(database): implement cleanDatabase function to streamline database cleanup; add new seeders for security profiles, permissions, and enhance user seeding logic

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,45 +1,49 @@
 import { PrismaClient } from '@prisma/client';
-import { seedUserStates } from './seeders/user-states.seeder';
-import { seedUsers } from './seeders/users.seeder';
-import { seedInstitutions } from './seeders/institutions.seeder';
-import { seedBranches } from './seeders/branches.seeder';
-import { seedMedicalAppointmentStates } from './seeders/medical_appointment_state';
-import { seedAdministrationRoutes } from './seeders/administration_route';
-import { seedFileTypes } from './seeders/file_type';
-import { seedRelationshipTypes } from './seeders/relationship_type';
+import { cleanDatabase } from './clean-database';
+import {
+    seedUsers,
+    seedBranches,
+    seedFileTypes,
+    seedUserStates,
+    seedInstitutions,
+    seedSecurityProfiles,
+    seedRelationshipTypes,
+    seedSecurityPermissions,
+    seedAdministrationRoutes,
+    seedSecurityPermisoProfiles,
+    seedMedicalAppointmentStates,
+} from './seeders';
 
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.medical_appointment.deleteMany();
-    await prisma.user_state.deleteMany();
-    await prisma.branch.deleteMany();
-    await prisma.institution.deleteMany();
-    await prisma.user.deleteMany();
-    await prisma.medical_appointment_state.deleteMany();
-    await prisma.administration_route.deleteMany();
-    await prisma.file_type.deleteMany();
-    await prisma.relationship_type.deleteMany();
-
+    await cleanDatabase(prisma);
 
     const now = new Date();
 
+    // === Catálogos base ===
     await seedUserStates(prisma, now);
-    await seedUsers(prisma);
-    await seedInstitutions(prisma, now);
-    await seedBranches(prisma, now);
     await seedMedicalAppointmentStates(prisma, now);
     await seedAdministrationRoutes(prisma, now);
     await seedFileTypes(prisma, now);
     await seedRelationshipTypes(prisma, now);
 
-    console.log('✅ Seed ejecutado correctamente.');
+    // === Entidades principales ===
+    await seedInstitutions(prisma, now);
+    await seedBranches(prisma, now);
+    await seedUsers(prisma);
 
+    // === Seguridad ===
+    await seedSecurityPermissions(prisma);
+    await seedSecurityProfiles(prisma);
+    await seedSecurityPermisoProfiles(prisma);
+
+    console.log('✅ Seed ejecutado correctamente.');
 }
 
 main()
     .catch((e) => {
-        console.error(e);
+        console.error('❌ Error ejecutando seed:', e);
         process.exit(1);
     })
     .finally(async () => {
