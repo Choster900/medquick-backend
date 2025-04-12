5fa6419f801a7b5ba967ff37233b04d214c83b8e
Choster900 <16adonaysergio@gmail.com>
2025-04-12 13:09:18 -0600

    feat(prescriptions): implement create prescription functionality with validation; add DTOs for prescription and prescription items; enhance prescriptions service with error handling and database integration

---
+++ src/prescriptions/interfaces/prescription-items.interface.ts
@@ -0,0 +1,12 @@
+
+
+export interface PrescriptionItems {
+    prescriptionId : string
+    administrationRouteId : number
+    prescriptionItemMedicationName : string
+    prescriptionItemDosage : string
+    prescriptionItemFrequency : string
+    prescriptionItemDuration : string
+    prescriptionItemUnit : string
+    prescriptionItemItemNotes : string
+}
