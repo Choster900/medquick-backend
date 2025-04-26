ab7b86ba54e0c3c09ac35d6722147fd0331d1d03
Choster900 <16adonaysergio@gmail.com>
2025-04-26 13:38:50 -0600

    feat(users): enhance user retrieval with detailed profile information and implement UUID validation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { auth_user_profile, security_permiso_profile } from './../../node_modules/.prisma/client/index.d';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+export class UsersService extends PrismaClient implements OnModuleInit {
 
-  findAll() {
-    return `This action returns all users`;
-  }
+    onModuleInit() {
+        this.$connect()
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    create(createUserDto: CreateUserDto) {
+        return 'This action adds a new user';
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    findAll() {
+        return `This action returns all users`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+    findOne(id: string) {
+        return this.user.findFirst({
+            select: {
+                user_id: true,
+                user_gender: true,
+                user_first_name: true,
+                user_second_name: true,
+                user_third_name: true,
+                user_first_lastname: true,
+                user_second_lastname: true,
+                user_third_lastname: true,
+                user_email: true,
+                user_dui: true,
+                user_birthdate: true,
+                user_phone_number: true,
+                user_address: true,
+                user_state: {
+                    select: {
+                        user_state_id: true,
+                        user_state_name: true,
+                        user_state_description: true
+                    }
+                },
+                auth_user_profile: {
+                    select: {
+                        security_profile: {
+                            select: {
+                                security_profile_profile_name: true,
+                                security_profile_profile_description: true
+                            }
+                        }
+                    }
+                }
+            },
+            where: {
+                user_id: id
+            },
+        });
+    }
+
+    update(id: number, updateUserDto: UpdateUserDto) {
+        return `This action updates a #${id} user`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} user`;
+    }
 }
