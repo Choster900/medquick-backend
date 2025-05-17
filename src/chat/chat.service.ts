419bd3ac4810569a067c35ab5883c4268e020a16
Choster900 <16adonaysergio@gmail.com>
2025-05-16 20:03:37 -0600

    feat: refactor saveMessage method to ensure chat creation and improve error handling

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -52,37 +52,42 @@ export class ChatService extends PrismaClient implements OnModuleInit {
     }
 
     async saveMessage(from: string, to: string, message: string) {
-        // Buscar si el chat ya existe
-        let chat = await this.chat.findFirst({
-            where: {
-                chat_user_id: from,
-                chat_doctor_id: to,
-            },
-        });
 
-        if (!chat) {
-            chat = await this.chat.create({
-                data: {
+        try {
+            // Buscar si el chat ya existe
+            let chat = await this.chat.findFirst({
+                where: {
                     chat_user_id: from,
                     chat_doctor_id: to,
                 },
             });
+
+            if (!chat) {
+                chat = await this.chat.create({
+                    data: {
+                        chat_user_id: from,
+                        chat_doctor_id: to,
+                    },
+                });
+            }
+
+            const savedMessage = await this.message.create({
+                data: {
+                    message_user_id: from,
+                    message_chat_id: chat.chat_id,
+                    message_content: message,
+                    message_status_id: 1,
+                },
+            });
+
+
+
+            return buildSuccessResponse(savedMessage, 'Chat guardado');
+
+        } catch (error) {
+            return buildErrorResponse(error.message, error.status, 500);
         }
 
-        const savedMessage = await this.message.create({
-            data: {
-                message_user_id: from,
-                message_chat_id: chat.chat_id,
-                message_content: message,
-                message_status_id: 1,
-            },
-        });
-
-        return {
-            success: true,
-            message: 'Mensaje guardado exitosamente',
-            data: savedMessage,
-        };
     }
 
     async getAllChats(userId: string) {
